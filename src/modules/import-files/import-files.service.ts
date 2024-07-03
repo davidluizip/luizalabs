7dee64ac38b8a5577e6c77e1bac5de1c739ce343
David Oliveira <davidluizip@gmail.com>
2024-07-03 11:44:10 -0300

    chore: import files

--- src/modules/import-files/import-files.service.ts
+++ src/modules/import-files/import-files.service.ts
@@ -8,6 +8,11 @@ import {
 export class ImportFilesService {
   constructor(private readonly readDataFromFile: ReadDataFromFileUseCase) {}
 
+  /**
+   * Método importFiles que vincula o caminho do arquivo especificado ao método run de ReadDataFromFileUseCase.
+   * @param filePath Caminho do arquivo a ser importado.
+   * @returns Uma função vinculada que pode ser executada para iniciar a importação do arquivo especificado.
+   */
   importFiles = ({ filePath }: IReadDataFromFileUseCase) =>
     this.readDataFromFile.run.bind({ filePath });
 }
